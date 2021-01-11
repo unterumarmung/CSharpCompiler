1fb20e06162b1778227c3581c7aff8e2f0a28dbd
Daniil <umarmungenfuerimmer@gmail.com>
2021-01-11 19:43:14 +0300

    Добавлен тест на операции с int

---
+++ Compiler/Tests/Ints.cs
@@ -0,0 +1,15 @@
+namespace Ints
+{
+    public class M
+    {
+        public static void Main()
+        {
+            var Console = new System.Console;
+
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(1 - 2);
+            Console.WriteLine(4 / 2);
+            Console.WriteLine(3 * 4);
+        }
+    }
+}

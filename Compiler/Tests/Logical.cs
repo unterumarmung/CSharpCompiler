052d829ef8f183328d08e346f4a29ff39086032c
Daniil <umarmungenfuerimmer@gmail.com>
2021-01-11 19:43:28 +0300

    Добавлен тест на && и ||

---
+++ Compiler/Tests/Logical.cs
@@ -0,0 +1,31 @@
+namespace Logical
+{
+    public class M
+    {
+        public System.Console Console = new System.Console;
+        public bool First()
+        {
+            Console.WriteLine("First");
+            return false;
+        }
+
+        public bool Second()
+        {
+            Console.WriteLine("Second");
+            return true;
+        }
+        public static void Main()
+        {
+            M m = new M;
+            if (m.First() && m.Second())
+            {
+                
+            }
+
+            if (m.Second() || m.Second())
+            {
+                
+            }
+        }
+    }
+}
